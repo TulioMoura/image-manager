6dc8a3b89639b0d8980bfcfa0b2614f64da44bfe
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-12-09 02:13:56 +0000

    created class imageDescriptor

---
+++ image-manager/src/main/java/com/tdm/imagemanager/classes/ImageDescriptor.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.lang.Exception;
+import java.lang.String;
+
+public class ImageDescriptor{
+    private String uuid;
+    private Date uploadDate;
+    private ArrayList<String> characteristics;
+    private Image thumbnail;
+
+    public String getId(){
+        return uuid;
+    }
+    public Date getDate(){
+        return uploadDate;
+    }
+    public ArrayList<String> getCharacteristics(){
+        return characteristics;
+    }
+    public Image getThumbnail{
+        return thumbnail;
+    }
+
+    public void addCharacteristic(String charcateristic){
+        characteristics.add(charcateristic);
+    }
+
+    public void removeCharacteristic(int index){
+        characteristics.remove(index);
+    }
+
+    public void removeAllCharacteristics(){
+        characteristics = new ArrayList<String>;
+    }
+
+    public void setThumbnail(Image thumbnail){
+        this.thumbnail = thumbnail;
+    }
+}
