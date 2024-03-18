05cfd59c62bbccd10fbefcf2e2d23668c6c618f0
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-03-18 03:17:21 +0000

    updated and normalized sql initial script

--- scripts/sqlscript.sql
+++ scripts/sqlscript.sql
@@ -1,30 +1,30 @@
-CREATE TABLE imageDescriptor(uuid TEXT(36) NOT NULL,
+CREATE TABLE img_descriptor(uuid TEXT(36) NOT NULL,
 uploadDate INTEGER NOT NULL,
 characteristics TEXT(256) NOT NULL,
-CONSTRAINT imageDescriptor_pk PRIMARY KEY (uuid));
-CREATE TABLE galleries (
+CONSTRAINT img_descriptor_pk PRIMARY KEY (uuid));
+CREATE TABLE gallery (
 	name TEXT(64),
 	id TEXT(36),
 	created_at INTEGER,
-	CONSTRAINT galleries_PK PRIMARY KEY (id)
+	CONSTRAINT gallery_PK PRIMARY KEY (id)
 );
-CREATE TABLE categories (
+CREATE TABLE category (
 	nome TEXT,
-	inclusao INTEGER,
-	CONSTRAINT categories_PK PRIMARY KEY (nome)
+	created_at INTEGER,
+	CONSTRAINT category_PK PRIMARY KEY (nome)
 );
-CREATE TABLE imageGallery (
+CREATE TABLE image_gallery (
 	image_id TEXT(36),
 	gallery_id text(36),
-	CONSTRAINT imageGallery_PK PRIMARY KEY (image_id,gallery_id),
-	CONSTRAINT imageGallery_FK FOREIGN KEY (image_id) REFERENCES imageDescriptor(uuid),
-	CONSTRAINT imageGallery_FK_1 FOREIGN KEY (gallery_id) REFERENCES galleries(id)
+	CONSTRAINT ImageGallery_PK PRIMARY KEY (image_id,gallery_id),
+	CONSTRAINT ImageGallery_FK FOREIGN KEY (image_id) REFERENCES img_descriptor(uuid),
+	CONSTRAINT ImageGallery_FK_1 FOREIGN KEY (gallery_id) REFERENCES gallery(id)
 );
-CREATE TABLE imageCategories (
-	category TEXT(36),
+CREATE TABLE image_categories (
+	category_name TEXT(36),
 	image_id TEXT(36),
-	CONSTRAINT imageCategories_PK PRIMARY KEY (category,image_id),
-	CONSTRAINT imageCategories_FK FOREIGN KEY (image_id) REFERENCES imageDescriptor(uuid),
-	CONSTRAINT imageCategories_FK_1 FOREIGN KEY (category) REFERENCES categories(nome)
+	CONSTRAINT imageCategories_PK PRIMARY KEY (category_name,image_id),
+	CONSTRAINT imageCategories_FK FOREIGN KEY (image_id) REFERENCES img_descriptor(uuid),
+	CONSTRAINT imageCategories_FK_1 FOREIGN KEY (category_name) REFERENCES category(nome)
 );
 
