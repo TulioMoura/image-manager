bb5ba2ab9dd4d1f10e56ddab1e4d0159e15cae6d
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-04-19 14:50:19 -0300

    created galleries controller

---
+++ src/main/java/com/tdm/imagemanager/controllers/galleriesController.java
@@ -0,0 +1,58 @@
+package com.tdm.imagemanager.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tdm.imagemanager.DAO.implementations.SQL.galleriesDaoSQL;
+import com.tdm.imagemanager.DAO.interfaces.galleriesDaoInterface;
+import com.tdm.imagemanager.classes.Gallery;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+public class galleriesController {
+    static final galleriesDaoInterface galleryDao = new galleriesDaoSQL();
+    @GetMapping("/galleries")
+    public ArrayList<Gallery> getGalleries(){
+        try{
+            return galleryDao.getAllGalleries();
+        }
+        catch(Exception exe){
+            System.out.println(exe);
+            return null;
+        }
+        
+    } 
+    @GetMapping("/gallery")
+    public Gallery getOneGallery(@RequestParam("id") String id){
+        try {
+            return galleryDao.getOneGallery(id);
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    @PostMapping("/gallery")
+    public Gallery addGallery(@RequestBody Gallery gallery){
+        try {
+            galleryDao.addGallery(gallery);
+            return galleryDao.getOneGallery(gallery.getId());
+
+        } catch (Exception e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+    @DeleteMapping("/gallery")
+    public void removeGallery(@RequestParam("id") String id){
+        try {
+            galleryDao.removeGallery(id);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+    }
+}
