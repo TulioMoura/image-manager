c307d6df33d20927bb312ed35e3494b069ac69cb
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-04-19 14:18:05 -0300

    renamed image_raw class

---
+++ src/main/java/com/tdm/imagemanager/classes/imageRaw.java
@@ -0,0 +1,22 @@
+package com.tdm.imagemanager.classes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Base64;
+class image_raw {
+    static Path decode(String encodedImage){
+        try{
+            byte[] decodedBytes = Base64.getDecoder().decode(encodedImage);
+        Path temporaryPath = Paths.get("tmp/");
+        return Files.write(temporaryPath,decodedBytes, StandardOpenOption.CREATE_NEW);
+        }
+        catch(IOException ex){
+            System.out.println("cannot write file");
+            System.out.println(ex);
+            return null;
+        }
+    }    
+}
